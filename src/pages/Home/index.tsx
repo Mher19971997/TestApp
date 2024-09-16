09d5276df1d6d45e2fa1e7065c0dc6a3b2c7dd0d
unknown <mher.melqonyan.mher123@gmail.com>
2024-09-16 18:35:25 +0400

    Added table

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,16 @@
 import { CSVDownloadBtn } from "./components/ CSVDownloadBtn";
 import { MenuComponent } from "./components/Menu/Menu";
-import data from '../../DATA.json'
-const Home = () => {
-    return (
-        <>
-            <MenuComponent />
-            <CSVDownloadBtn datas={data} downloadLoading={false} />
-        </>
-    )
-}
+import data from "../../DATA.json";
+import Table from "../../components/Table";
 
+const Home = () => {
+  return (
+    <>
+      <MenuComponent />
+      <CSVDownloadBtn datas={data} downloadLoading={false} />
+      <Table />
+    </>
+  );
+};
 
 export default Home;
